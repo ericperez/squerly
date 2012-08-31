c8b77731a123fe2f7378366aa08eed308e67e46f
Eric Perez <eric.perez@yahoo.com>
2012-08-30 21:33:42 -0600

    Removed JSON_NUMERIC_CHECK from JSON exporter since it doesn't play nicely with certain date formats

--- helpers/export_json.php
+++ helpers/export_json.php
@@ -17,13 +17,21 @@ class Export_Json implements Export_Interface {
   * Renders 2D array in JSON format
   * 
   * @param array $data 2D associative array of data to be exported
-  * @param string $filename Name of file that will hold the CSV results
+  * @param string $filename Name of file that will hold the JSON results
   * @param array $config Array of configuration settings (currently unused)
   * @return string JSON-encoded string representing input data
   *
   */
   public static function render(array $data, $filename = NULL, $config = array()) {
-    $json = json_encode($data, JSON_NUMERIC_CHECK);
+    //Make sure 'integer' string values are converted into actual integers
+    foreach($data as &$row) {
+      foreach($row as $k => &$v) {
+        if(is_numeric($v) && (string) intval($v) === $v) {
+          $v = (int) $v;
+        }
+      }
+    }
+    $json = json_encode($data); //, JSON_NUMERIC_CHECK);
     return $json;
   }
 
