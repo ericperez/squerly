9c419dc8e7bf6330c551958f0e01b5962c4be2a2
Eric Perez <eric.perez@yahoo.com>
2012-09-16 14:52:42 -0600

    Updated SQLite report table definition

--- sql/sqlite_install_script.sql
+++ sql/sqlite_install_script.sql
@@ -19,20 +19,3 @@ CREATE TABLE 'report' (
 );
 
 COMMIT;
-
-
-CREATE TABLE 'report' (
-  'id' INTEGER PRIMARY KEY ASC NOT NULL ,
-  'name' varchar(255) NOT NULL,
-  'type' varchar(63) NOT NULL,
-  'notes' varchar(255) DEFAULT '',
-  'keywords' varchar(255) DEFAULT '',
-  'hidden_from_ui' BOOLEAN DEFAULT false,
-  'enabled' BOOLEAN DEFAULT true,
-  'db_adapter' VARCHAR(255),
-  'input_data_uri' VARCHAR(4095),
-  'query' text,
-  'postprocess_code' text,
-  'created_at' DATETIME NOT NULL,
-  'updated_at' DATETIME NOT NULL
-);
