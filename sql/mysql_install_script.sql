9ad621979b16390cdf65d58b31ef0082e9fe2a7f
Eric Perez <zerep@rocketmail.com>
2014-02-23 21:43:34 -0700

    Removed composer.phar from git; removed old sql install script; updated to newer sql install scripts for MySQL and SQLite

--- sql/mysql_install_script.sql
+++ sql/mysql_install_script.sql
@@ -1,22 +1,31 @@
-
-
 -- Primary Report Table
 CREATE TABLE `report` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
+  `description` VARCHAR(255) DEFAULT '',
+  `enabled` boolean DEFAULT 1,
   `type` VARCHAR(63) NOT NULL,
-  `data_source` VARCHAR(255),
-  `input_data_uri` VARCHAR(4095),
+  `db_adapter` VARCHAR(255),
+  `input_data_uri` VARCHAR(1023),
   `query` TEXT,
   `postprocess_code` TEXT,
-  `description` VARCHAR(255) DEFAULT '',
   `keywords` VARCHAR(255) DEFAULT '',
-  `hidden_from_ui` BOOLEAN DEFAULT 0,
-  `enabled` BOOLEAN DEFAULT 1,
-  `default_refresh_time` INT DEFAULT 0,
-  `default_cache_time` INT DEFAULT 0,
-  'default_export_context' VARCHAR(255) DEFAULT '',
+  `hidden_from_ui` BOOLEAN DEFAULT TRUE,
   `form_method` VARCHAR(4) DEFAULT 'POST',
+  `created_at` DATETIME DEFAULT NULL,
+  `updated_at` TIMESTAMP DEFAULT NOW(),
+  PRIMARY KEY (`id`)
+) ENGINE=innoDB;
+
+
+-- Saved Report Form Configurations
+CREATE TABLE `saved_report` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` VARCHAR(1023) DEFAULT '',
+  `enabled` boolean DEFAULT 1,
+  `report_id` INTEGER NOT NULL,
+  `input_values` varchar(4095),
   `created_at` DATETIME DEFAULT NULL,
   `updated_at` TIMESTAMP DEFAULT NOW(),
   PRIMARY KEY (`id`)
--- sql/sqlite_install_script.sql
+++ sql/sqlite_install_script.sql
@@ -7,15 +7,12 @@ CREATE TABLE 'report' (
   'description' VARCHAR(255) DEFAULT '',
   'enabled' boolean DEFAULT 1,
   'type' VARCHAR(63) NOT NULL,
-  'data_source' VARCHAR(255),
-  'input_data_uri' VARCHAR(4095),
+  'db_adapter' VARCHAR(255),
+  'input_data_uri' VARCHAR(1023),
   'query' TEXT,
   'postprocess_code' TEXT,
   'keywords' VARCHAR(255) DEFAULT '',
   'hidden_from_ui' BOOLEAN DEFAULT 0,
-  'default_refresh_time' INT DEFAULT 0,
-  'default_cache_time' INT DEFAULT 0,
-  'default_export_context' VARCHAR(255) DEFAULT '',
   'form_method' VARCHAR(4) DEFAULT 'POST',
   'created_at' DATETIME DEFAULT NULL,
   'updated_at' DATETIME DEFAULT (DATETIME('now','localtime'))
@@ -29,34 +26,6 @@ CREATE TABLE 'saved_report' (
   'enabled' boolean DEFAULT 1,
   'report_id' INTEGER NOT NULL,
   'input_values' varchar(4095),
-  'created_by' VARCHAR(255), 
-  'created_at' DATETIME DEFAULT NULL,
-  'updated_by' VARCHAR(255),
-  'updated_at' DATETIME DEFAULT (DATETIME('now','localtime'))
-);
-  'created_at' DATETIME DEFAULT NULL,
-  'updated_at' DATETIME DEFAULT (DATETIME('now','localtime'))
-);
-
-
--- Email distribution lists for Reports
-CREATE TABLE 'email_distribution_list' (
-  'id' INTEGER PRIMARY KEY ASC NOT NULL,  
-  'name' varchar(255) NOT NULL,
-  'description' VARCHAR(255) DEFAULT '',
-  'enabled' boolean DEFAULT true,
-  'email_recipients' varchar(4095) NOT NULL,
-  'created_at' DATETIME DEFAULT NULL,
-  'updated_at' DATETIME DEFAULT (DATETIME('now','localtime'))
-);
-
--- Email Schedules for Reports
-CREATE TABLE 'email_schedule' (
-  'id' INTEGER PRIMARY KEY ASC NOT NULL,  
-  'name' varchar(255) NOT NULL,
-  'description' VARCHAR(255) DEFAULT '',
-  'enabled' boolean DEFAULT true,
-  'email_schedule' varchar(1023) NOT NULL,[]
   'created_at' DATETIME DEFAULT NULL,
   'updated_at' DATETIME DEFAULT (DATETIME('now','localtime'))
 );
